d78519ce43914beb15cf570d12c0542a56adb7ea
Shrinivas-Nilajagi <shrinivasnilajagi35@gmail.com>
2025-11-07 00:21:09 +0530

    comminting the breakpoint related git

---
+++ data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+file_path = "C:/Users/Shrinivas/OneDrive/Desktop/data.xlsx"
+
+# To read a specific sheet
+df = pd.read_excel(file_path, sheet_name="Sheet1")
+
+print(df.head())
